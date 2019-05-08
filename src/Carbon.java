8616ec3f656f14228c681d9e6af4eec6079a172d
Banks - Mholi <mholi.mncube@gmail.com>
2019-05-08 22:14:28 +0200

    update

--- src/Carbon.java
+++ src/Carbon.java
@@ -18,11 +18,12 @@ public class Carbon extends Thread {
 	    	sharedMethane.mutex.acquire(); //first carbon thread to methane blocks other threads to bond
 	    	sharedMethane.addCarbon(); //makes a bond C if C thread gets there before 4 H threads
 	    	//conditions for group to bond
-	    	if(sharedMethane.getHydrogen() >=4){
-	    		sharedMethane.carbonQ.release();
-	    		sharedMethane.removeCarbon(1);
+	    	if(sharedMethane.getHydrogen() >=4 ){
 	    		sharedMethane.hydrogensQ.release();
 	    		sharedMethane.removeHydrogen(4);
+	    		sharedMethane.carbonQ.release();
+	    		sharedMethane.removeCarbon(1);
+	    		
 	    	}
 	    	else{
 	    		sharedMethane.mutex.release();
